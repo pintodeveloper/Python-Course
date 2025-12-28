8da87045c05fcfde2a8fc6419b2c342d8ca83804
developerpinto <developerpinto@gmail.com>
2025-12-28 08:22:28 -0500

    feat: add new list

--- List.py
+++ List.py
@@ -1,3 +1,22 @@
 workers = ["Ana","Pinto","Coco77","Lorena"]
 
 print(workers)
+
+print(len(workers))
+
+workers.append("Felipe77")
+
+print(len(workers))
+
+
+print(workers[2])
+
+print(workers[-1])
+
+
+newWorkers = ["Andry","Messi","CR7","Luiz Dias","James"]
+
+workers.extend(newWorkers)
+
+
+print(workers)
