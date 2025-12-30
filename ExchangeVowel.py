d8598b40504b14eca38ac16acb3b4d9887ba22f6
developerpinto <developerpinto@gmail.com>
2025-12-29 20:27:58 -0500

    feat: add new practice the list

--- ExchangeVowel.py
+++ ExchangeVowel.py
@@ -11,6 +11,29 @@ vowel.append("pinto")
 
 print(vowel)
 
-del vowel[1:5]
+print(vowel[1:5])
+
+print(vowel[::-1])
+
+#del vowel[-1]
 
 print(vowel)
+
+print(vowel[::2])
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+#print(numbers[::3]) # every 3rd element [1,4,7,10]
+
+#print(numbers[1::3]) # from index 1, every 3rd element [2,5,8]
+
+#print(numbers[2::4]) # from index 2, every 4th element [3,7]
+
+#print(numbers[::-2]) # reverse every 2nd element [10,8,6,4,2]
+
+print(numbers[3::3]) # from index 3, every 3rd element [4,7,10,13]
+
+print(numbers[4::4]) # 5 9 13
+
+print(numbers[5:12:2]) # from index 5 to 11, every 2nd element [6,8,10,12]
