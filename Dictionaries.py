c61a1c3e4b28f74edd5affc19b51eceb4e791212
developerpinto <developerpinto@gmail.com>
2025-12-31 23:33:44 -0500

    feat: add new practice the Dictionary

---
+++ Dictionaries.py
@@ -0,0 +1,38 @@
+person = {
+    "age": 20,
+    "name": 'Laura Michel Garcia uribe',
+    "Nacionalidad": 'Colombiana',
+    "Estado civil": 'Soltera',
+    "Gustos": 'Sabrosines',
+    77: 'felipe77',
+    'matriz': ['7','78','48','48','89']
+}
+
+#print(person)
+
+
+
+
+
+del person["age"]
+
+#print(person['matriz'])
+
+
+estados = ['Norte De santander', 'Santander', 'Valle del cauca', 'Bogota']
+
+
+citys = {
+    
+    estados[0]: 'Ocaña',
+    estados[1]: 'Bucaramanga',
+    estados[2]: 'Cauca',
+    estados[3]: 'Cundinamarca',
+    
+}
+
+
+print(citys[estados[0]])
+
+
+
