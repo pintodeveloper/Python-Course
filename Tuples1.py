9f079841c5c6a40cab40ecbb30dc1954ea8f1b58
developerpinto <developerpinto@gmail.com>
2025-12-31 22:49:27 -0500

    feat: add practice the tuples

---
+++ Tuples1.py
@@ -0,0 +1,16 @@
+MisDates = ("Pinto",'18','02','2005')
+
+print('La tupla contiene: ',MisDates)
+
+name, day, month, YearOld = MisDates
+
+print(YearOld)
+
+
+ListDates = list(MisDates)
+
+print(ListDates)
+
+
+
+print("Pinto" in MisDates)
