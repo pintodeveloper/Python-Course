fd913f3f8001f39dc408026e6e1fa67bbaabe1f8
pintodeveloper <pintodeveloper@gmail.com>
2026-01-01 12:18:17 -0500

    feat: working with dictionary

--- Dictionaries3.py
+++ Dictionaries3.py
@@ -13,3 +13,23 @@ print(tel)
 sorted(tel)
 
 print(tel)
+
+
+
+
+############Notations differens
+
+newDict = dict([
+    ('sape', 4139),
+    ('guido', 4127),    
+    ('jack', 4098)
+    ])
+
+print(newDict)
+
+newList2 = {x: x**2 for x in (2,4,6)}
+
+print(newList2)
+
+newList3 = dict(coco=77,pinto=77,felipe=77)
+print(newList3)
