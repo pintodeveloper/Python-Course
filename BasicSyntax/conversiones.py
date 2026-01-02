417cad68e886d56547d8a1ea07d22c18ea48d266
pintodeveloper <pintodeveloper@gmail.com>
2026-01-01 22:29:33 -0500

    feat: add convertines for dates

---
+++ BasicSyntax/conversiones.py
@@ -0,0 +1,14 @@
+a = "77"
+b = "0"
+
+nf= int(a+b)
+nf1 = int(float(a)-float(b))
+
+#print(nf1)
+
+print("Mi number favorite is " + str(a))
+
+employees = ["Ana","Paola","Lucia","Nata","felipe"]
+
+print(" - ".join(employees))
+
