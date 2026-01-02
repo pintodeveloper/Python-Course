e2d1dfc270685fe03b16c4d07cd3e1bab72eafea
pintodeveloper <pintodeveloper@gmail.com>
2026-01-02 14:01:45 -0500

    feat: add conditionals

---
+++ Not-In.py
@@ -0,0 +1,12 @@
+languague_trim1 = "Java, Python, PHP, C#, HTML!, JavaScript"
+
+
+if("Kotlin" not in languague_trim1):
+    print("Hace falta que agreguen a kotlin")
+else:
+    print("Kotlin ya se encuentra en la lista")
+    
+
+if(languague_trim1 not in "c++"):
+    print("No se encuentra en la lista C++, rcomendacion Agregarlo")
+else: print("Ya se encuentra en la lista")
