106ebf208e37ca15ec7945664d316cde9529128b
pintodeveloper <pintodeveloper@gmail.com>
2026-01-01 21:55:36 -0500

    feat: add complement the functions

--- BasicSyntax/Functions.py
+++ BasicSyntax/Functions.py
@@ -1,9 +1,21 @@
-def callMessage():
-    showMessage()
-    
 
-def showMessage():
-    print(77)
+def returnMessage():
+    return "Funcion retornable"
 
 
-showMessage()
+
+
+def showMessagePersonalizado(message):
+    return message
+
+
+def sumNumbers(m,n1,n2):
+    return m + str(n1+n2)
+
+m = "felipe pinto es un crack"
+
+
+M = showMessagePersonalizado(m)
+#print(M)
+
+print(sumNumbers("La suma de es: ",70,7))
