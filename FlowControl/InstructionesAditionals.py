8b65a2188ca5f3e0e26726d5ebc049e44de5833e
pintodeveloper <pintodeveloper@gmail.com>
2026-01-03 13:00:04 -0500

    feat: add aditionals the for bucless

---
+++ FlowControl/InstructionesAditionals.py
@@ -0,0 +1,33 @@
+name = "Andrey Felipe Pinto Uribe"
+
+#pass and continue
+count = 0
+
+for i in name:
+    if i == " ":
+        continue
+    count+=1
+    
+print(count)
+
+##pass
+
+
+class felipe77:
+    pass
+   
+##else
+
+email = input("Ingresa tu correo: ")
+
+
+for i in email:
+    if i == "@":
+        arroba = True
+        break
+
+else:
+    arroba = False
+    
+print(arroba)
+   
