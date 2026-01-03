414311527b0af6c7c378adc58836a55e4e42a633
pintodeveloper <pintodeveloper@gmail.com>
2026-01-03 11:23:11 -0500

    feat: add new method the numbers primes

--- FlowControl/ExerciseBucleFor.py
+++ FlowControl/ExerciseBucleFor.py
@@ -12,4 +12,23 @@ for i in range(number1,number2+1):
     if(count==2):
         print(i)   
 
+
+#############Enfoque 2
+
+num1 = int(input("Introduce el primer numero, por favor: "))
+num2 = int(input("Introduce el segundo numero, por favor: "))
+
+
+def is_prime(number):
+    for n in range(2,number):
+        if number % n == 0:
+            return False
+    print(str(number)+" es primo")
+    return True
+
+for i in range(num1,num2):
+    is_prime(i)
+    
+    
+    
     
