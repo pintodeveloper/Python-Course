c71bcc191fabfc60f8e1ebbf26d37ee3b8f6e099
pintodeveloper <pintodeveloper@gmail.com>
2026-01-08 18:04:54 -0500

    feat: add modified

--- POO/poo2.py
+++ POO/poo2.py
@@ -1,31 +1,43 @@
 class Person:
-    name = ""
+    __name = ""
     lastName = ""
-    age = 0
+    __age = 0
     gender = "sin definir"
 
-    def __init__(self,name,lastName,age,gender):
-        self.name = name
+    def __init__(self,name,lastName,gender):
+        self.__name = name
         self.lastName = lastName
-        self.age = age
+
         self.gender = gender
 
+    def verifyAge(self,age):
+        if age < 0 or age > 150:
+            print("Error en la edad..")
+        else:
+            self.__age = age
+
+
     def walking(self):
-        return "La persona "+self.name+" Esta caminando"
+        return "La persona "+self.__name+" Esta caminando"
     
     def talking(self):
-        return "La persona "+self.name+" Esta caminando"
+        return "La persona "+self.__name+" Esta caminando"
 
     def programming(self):
-        return "La persona "+self.name+" Esta programando"
+        return "La persona "+self.__name+" Esta programando"
 
 
     def getDates(self):
-        return "El nombre es "+self.name+ \
-        " su apellido es "+self.lastName+" su edad es de "+str(self.age) + \
+        return "El nombre es "+self.__name+ \
+        " su apellido es "+self.lastName+" su edad es de "+str(self.__age) + \
         " su genero es "+self.gender
-    
 
-p1 = Person("felipe","pinto",20,"Masculino")
+
+print("####################################################")
+
+p1 = Person("felipe","pinto","Masculino")
+p1.verifyAge(-20)
+
 print(p1.getDates())
-print(p1.talking())
+
+print(p1.programming())
