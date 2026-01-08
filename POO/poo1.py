07de4e9306d73f890c584584dc4a65755e8960c0
pintodeveloper <pintodeveloper@gmail.com>
2026-01-08 14:58:04 -0500

    feat: add new tema the poo self

--- POO/poo1.py
+++ POO/poo1.py
@@ -2,11 +2,20 @@ class Car():
     wheels = 4
     longChassis = 260
     brodChassis = 130
+    torOff = False
 
     def startUp(self):
-        pass
+        self.torOff = True
+    
+    def statusCar(self):
+        if(self.torOff):
+            return "El carro esta funcionando"
+        else:
+            return "El carro no Arranca"
 
 mazda = Car()
 renault = Car()
 
-print(renault.wheels)
+#mazda.startUp()
+
+print("El estado del carro: ",mazda.statusCar())
