15414e8d2d522504ba7669af9c7f44f4d526dea0
pintodeveloper <pintodeveloper@gmail.com>
2026-01-12 10:39:02 -0500

    feat: add new concept the sobreescritura and super()

--- POO/Inheritance.py
+++ POO/Inheritance.py
@@ -23,12 +23,27 @@ class Person():
     
 class Student(Person):
     
+    def __init__(self, name, lastName, age,school):
+        #self.name = name
+        #self.lastName = lastName
+        #self.age = age
+        #El metodo Super(), sirve cuando hay sobre escritura y se necesita info del padre
+        
+        super().__init__(name, lastName, age)
+    
+        self.school = school
+    
+    def getDates(self):
+        return super().getDates() + " Escuela "+self.school
+    
     def studying(self):
         return "Estoy estudiando.."
     
 person1 = Person("Felipe","Pinto",20)
 
-student1 = Student("Andrey","Uribe",21)
+#queremos saber el colegio en el cual estudio.
+
+student1 = Student("Andrey","Uribe",21,"San Francisco")
 
 print(person1.getDates())
 print(student1.getDates())
