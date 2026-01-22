72e994adc1de1f7aad82bb374da583cb8813f4d7
pintodeveloper <pintodeveloper@gmail.com>
2026-01-22 17:14:52 -0500

    feat: add tema the modules python

---
+++ modules/functions_mathe.py
@@ -0,0 +1,9 @@
+def add(num1,num2):
+    print("La suma es: ",num1+num2)
+    
+def res(num1,num2):
+    print("La resta es: ",num1-num2)
+    
+def mul(num1,num2):
+    print("La multiplicacion es: ",num1*num2)
+    
---
+++ modules/work_modules.py
@@ -0,0 +1,12 @@
+#Existen formas para utilizar los modulos
+
+"""import functions_mathe
+
+functions_mathe.add(70,7)
+"""
+
+from functions_mathe import add
+from functions_mathe import *
+
+add(70,7)
+res(15,8)
