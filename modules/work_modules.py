7b7f3918e39a816c2394f9c7c97cbe84e102f2df
pintodeveloper <pintodeveloper@gmail.com>
2026-01-25 18:38:57 -0500

    feat: working with modules

--- modules/work_modules.py
+++ modules/work_modules.py
@@ -5,8 +5,9 @@
 functions_mathe.add(70,7)
 """
 
-from functions_mathe import add
-from functions_mathe import *
+##from modulesMa.functions_mathe import add
+from modulesMa.calculosBasicos.functions_mathe import *
 
 add(70,7)
 res(15,8)
+mul(11,7)
---
+++ modulesMa/otrosCalculos/potenciaAndRound.py
@@ -0,0 +1,5 @@
+def potencia(base,exp):
+    print("El resultado de la potencia es: ",base**exp)
+    
+def redondear(number):
+    print("El redondeado es: ",round(number))
