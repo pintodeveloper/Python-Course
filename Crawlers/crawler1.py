249c5ec7c058e71811d92debc961caf47d83fbce
pintodeveloper <pintodeveloper@gmail.com>
2026-02-27 23:08:09 -0500

    feat: add new thema the BeautifulSoup

---
+++ Crawlers/crawler1.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+miDoc ="""
+
+    <html>
+    <body>
+    <p> Este es el primer parrafo</p>
+    <p>Este es el segundo parrafo</>
+    </body>
+    </html>
+
+"""
+soup = BeautifulSoup(miDoc, 'html.parser')
+
+for parrafo in soup.find_all("p"):
+    print(parrafo)
+    
+print(soup)
