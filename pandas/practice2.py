8f69cf53ad2b3130f76701536c4c02517f66ee65
pintodeveloper <pintodeveloper@gmail.com>
2026-03-03 22:40:32 -0500

    feat: cargue datos del csv at programa practice 2

--- pandas/practice2.py
+++ pandas/practice2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-aguacates = pd.read_excel("C:\\Users\\andre\\Desktop\\pinto77\\python-course\\pandas\\Aguacate producción.xlsx")
+aguacates = pd.read_csv("C:\\Users\\andre\\Desktop\\pinto77\\python-course\\pandas\\Aguacate.csv")
+
 
 
 print(aguacates)
