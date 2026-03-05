87850374c3f647c95ff862c44e3affdd9a8b4760
pintodeveloper <pintodeveloper@gmail.com>
2026-03-04 22:57:25 -0500

    feat: read json

---
+++ pandas/practice3.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import requests
+
+
+dates_products = requests.get("https://dummyjson.com/products").json()
+
+#df = pd.read_json('C:\\Users\\andre\\Desktop\\pinto77\\python-course\\pandas\\fichero_covid\\fichero_covid.json')
+
+#print(dates_products.keys())
+#print(dates_products.get("products"))
+
+
+products = pd.DataFrame.from_dict(dates_products.get("products"))
+
+
+print(products)
