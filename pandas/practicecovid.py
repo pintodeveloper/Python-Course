644a2597988b733a18af2fd59f769670f13a3e75
pintodeveloper <pintodeveloper@gmail.com>
2026-03-15 22:20:17 -0500

    relax> 77

--- pandas/practicecovid.py
+++ pandas/practicecovid.py
@@ -3,4 +3,6 @@ import pandas as pd
 import requests
  
 covid = pd.read_json("C:\\Users\\andre\\Desktop\\pinto77\\python-course\\pandas\\fichero_covid\\fichero_covid.json")
-print(77)
+print(77
+      )
+print("la rachita")
